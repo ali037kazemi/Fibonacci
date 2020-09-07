1bcd676d5922e34312edf94add8040e626e0c1ee
Ali Kazemi <Kazemi037ali@gmail.com>
2020-09-07 16:32:07 +0430

    First draw of form & add method GetFiboList

--- WinFormFibonacci/WinFormFibonacci/Form1.Designer.cs
+++ WinFormFibonacci/WinFormFibonacci/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace WinFormFibonacci {
-    partial class Form1 {
+    partial class FormFibonacci {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -26,13 +26,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(234, 12);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(237, 411);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "اعداد فیبوناچی";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 93);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(107, 23);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(87, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "تعداد اعداد دنباله را وارد کنید:";
+            // 
+            // FormFibonacci
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(484, 435);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FormFibonacci";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Fibonacci";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WinFormFibonacci/WinFormFibonacci/Form1.cs
+++ WinFormFibonacci/WinFormFibonacci/Form1.cs
@@ -9,10 +9,46 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinFormFibonacci {
-    public partial class Form1 : Form {
-        public Form1()
+    public partial class FormFibonacci : Form {
+
+        private static readonly List<Color> colors = new List<Color>
+        {
+            Color.Red, Color.Black, Color.Blue, Color.Purple, Color.Pink, Color.Green,
+            Color.Gray, Color.Yellow, Color.Orange, Color.Khaki, Color.Aqua, Color.Brown,
+            Color.Transparent, Color.Magenta
+        };
+
+        public FormFibonacci()
         {
             InitializeComponent();
+        }
+
+        public List<int> GetFiboList(int n)
+        {
+            List<int> numbers = new List<int>(n);
+
+            numbers.Add(1);
+            numbers.Add(1);
+            for (int i = 2; i < n; i++)
+            {
+                numbers.Add(numbers[i - 2] + numbers[i - 1]);
+            }
+
+            return numbers;
+        }
+
+        public Label GetLabelNumber(string numberText, int numberNo)
+        {
+            Label lblNumber = new Label();
+
+            lblNumber.Text = numberText;
+
+            int colorNumber = numberNo % colors.Count;
+            lblNumber.ForeColor = colors[colorNumber];
+
+            lblNumber.Location = new Point(200, 200 + numberNo * 10);
+
+            return lblNumber;
         }
     }
 }
